36bca053cf45f6e3f03a9b29b8508c4c47997c8c
yolandacr <73035520+yolandacr@users.noreply.github.com>
2021-12-06 13:47:15 +0100

    mensajes en consola terminado
    mensajes en consola terminado

--- javascript/FuncionesApariencia.js
+++ javascript/FuncionesApariencia.js
@@ -17,3 +17,18 @@ $(".checkbox2").click(function () {
     $("#fondo").css("opacity", "0.6");
   }
 });
+
+
+//funcion para que salgan los mensajes de console.log en un div consola.
+
+ if (typeof console  != "undefined") 
+    if (typeof console.log != 'undefined')
+        console.olog = console.log;
+    else
+        console.olog = function() {};
+
+console.log = function(message) {
+    console.olog(message);
+    $('#consola').append('<p>' + message + '</p>');
+};
+console.error = console.debug = console.info =  console.log 
